3c80cd5e06be43578004b72c30ccc6b9b6fbf171
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-13 01:12:35 +0530

    File Added

---
+++ Backend/Models/product.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    image_src: String,
+    productRefLinc: String,
+    rating: Number,
+    colour: String,
+    price : Number,
+    mprice: Number,
+    name: String,
+    shape: String,
+    gender: String,
+    style: String,
+    dimension:String,
+    productType: String,
+    productId : String,
+    userRated: Number,
+    quality: Number,
+    id: Number
+});
+
+const productModel = mongoose.model("product",productSchema)
+
+module.exports={ productModel };
