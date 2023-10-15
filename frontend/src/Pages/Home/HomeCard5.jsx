e8a6ec00f46a8cd0f8ddd9f0667e99391a1f241e
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-16 03:39:52 +0530

    File Added

---
+++ frontend/src/Pages/Home/HomeCard5.jsx
@@ -0,0 +1,53 @@
+import { Box, Grid, Text } from "@chakra-ui/react";
+import React from "react";
+
+
+
+export const HomeCard5 = () => {
+    return (
+        <Box w="85%" m="auto">
+            <Text fontSize="30px" pb="7" fontWeight="500" textAlign="center" >
+                Find The Perfect Fit
+            </Text>
+            <Grid
+            templateColumns={{
+                base: "repeat(1,1fr)",
+                md: "repeat(2,1fr)",
+                lg: "repeat(2,1fr)",
+                xl: "repeat(2,1fr)",
+                "2xl":"repeat(2,1fr)"
+            }}
+            >
+                <Box>
+                <Image
+            src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/eye-square10.jpg"
+            alt="img"
+            p="2"
+          />
+          <Image
+            src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/power-sun-square.jpg"
+            alt="img"
+            p="2"
+          />
+        </Box>
+        <Box>
+          <Image
+            src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/ce-square.jpg"
+            alt="img"
+            p="2"
+          />
+          <Image
+            src="https://static1.lenskart.com/media/desktop/img/Aug21/Desktop/sun-square.jpg"
+            alt="img"
+            p="2"
+          />
+          <Image
+            src="https://static1.lenskart.com/media/desktop/img/Nov20/25-Nov/Banner03_TileDesktop.jpg"
+            alt="img"
+            p="2"
+          />
+                </Box>
+            </Grid>
+        </Box>
+    )
+}
