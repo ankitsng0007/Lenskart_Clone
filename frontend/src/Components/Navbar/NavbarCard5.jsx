5bc8d305daab07141beabe9f12ad891324692faf
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-17 00:16:21 +0530

    File Added

--- frontend/src/Components/Navbar/NavbarCard5.jsx
+++ frontend/src/Components/Navbar/NavbarCard5.jsx
@@ -283,6 +283,188 @@ const NavbarCard5 = () => {
         </MenuList>
       </Menu>
 
+      <Menu>
+        <MenuButton
+          bg="#fbf9f7"
+          fontSize="15px"
+          fontWeight="600"
+          _hover={{
+            borderBottom: "4px solid teal"
+          }}
+        >
+          KIDS GLASSES
+        </MenuButton>
+
+        <MenuList
+          color="blackAlpha.900"
+          h="100%"
+          bg="whiteAlpha.800"
+          w="100%"
+          p="2"
+        >
+          <Link to="/products">
+            <Box>
+              <Grid
+                gridTemplateColumns="repeat(3, 1fr)"
+                justifyContent="center"
+                p="5"
+              >
+                <Box bg="whiteAlpha.900" h="250px" w="240px">
+                  <img
+                    className="navImg1"
+                    src="https://static1.lenskart.com/media/desktop/img/May22/glasses.jpg"
+                    alt="kidsIcon_1"
+                  />
+                  <Box mt="10px" textAlign="center" fontSize="lg">
+                    Eye Glasses
+                  </Box>
+                </Box>
+                <Box bg="whiteAlpha.900" h="250px" w="240px">
+                  <img
+                    className="navImg2"
+                    src="https://static1.lenskart.com/media/desktop/img/May22/computer-glasses.jpg"
+                    alt="kidsIcon_2"
+                  />
+                  <Box mt="10px" textAlign="center" fontSize="lg">
+                    Zero Power Computer Glasses
+                  </Box>
+                </Box>
+                <Box bg="whiteAlpha.900" h="250px" w="240px">
+                  <img
+                    className="navImg2"
+                    src="https://static1.lenskart.com/media/desktop/img/May22/Sunnies.jpg"
+                    alt="kidsIcon_3"
+                  />
+                  <Box mt="10px" textAlign="center" fontSize="lg">
+                    Sun Glasses
+                  </Box>
+                </Box>
+              </Grid>
+            </Box>
+          </Link>
+        </MenuList>
+      </Menu>
+
+      <Menu>
+        <MenuButton
+          bg="#fbf9f7"
+          fontSize="15px"
+          fontWeight="600"
+          _hover={{
+            borderBottom: "4px solid teal"
+          }}
+        >
+          CONTACT LENSES
+        </MenuButton>
+
+        <MenuList
+          color="blackAlpha.900"
+          h="400px"
+          bg="whiteAlpha.800"
+          p="5"
+          w="100%"
+        >
+          <Link to="/newproduct">
+            <Box>
+              <Grid gridTemplateColumns="repeat(5, 1fr)" p="1" w="100%">
+                <Flex direction="column" gap="6">
+                  <Box
+                    fontSize="md"
+                    fontWeight="bold"
+                    borderBottom="1px solid black"
+                    p="1"
+                  >
+                    Brands
+                  </Box>
+                  <Flex direction="column" gap="2" fontSize="md">
+                    <Box _hover={{ fontWeight: "bold" }}> Aqualens</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Bausch Lomb</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Johnson & Johnson</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Soflens</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Acuvue</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Alcon</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Air Optix</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Pure Vision</Box>
+                  </Flex>
+                </Flex>
+
+                <Flex direction="column" gap="6">
+                  <Box
+                    fontSize="md"
+                    fontWeight="bold"
+                    borderBottom="1px solid black"
+                    p="1"
+                  >
+                    Explore by Disposability
+                  </Box>
+                  <Flex direction="column" fontSize="md" gap="2">
+                    <Box _hover={{ fontWeight: "bold" }}> Monthly</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Day & Night</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Daily</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Yearly</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Bi-weekly</Box>
+                  </Flex>
+                </Flex>
+
+                <Flex direction="column" gap="6">
+                  <Box
+                    fontSize="md"
+                    fontWeight="bold"
+                    borderBottom="1px solid black"
+                    p="1"
+                  >
+                    Explore by Power
+                  </Box>
+                  <Flex direction="column" fontSize="md" gap="2">
+                    <Box _hover={{ fontWeight: "bold" }}>Spherical - CYL</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Spherical + CYL</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Cylindrical Power</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Toric Power</Box>
+                  </Flex>
+                </Flex>
+
+                <Flex direction="column" gap="6">
+                  <Box
+                    fontSize="md"
+                    fontWeight="bold"
+                    borderBottom="1px solid black"
+                    p="1"
+                  >
+                    Explore by Colors
+                  </Box>
+                  <Flex direction="column" fontSize="md" gap="2">
+                    <Box _hover={{ fontWeight: "bold" }}>Green</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Blue</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Brown</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Turquoise</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>View all colors</Box>
+                  </Flex>
+                </Flex>
+
+                <Flex direction="column" gap="6">
+                  <Box
+                    fontSize="md"
+                    fontWeight="bold"
+                    borderBottom="1px solid black"
+                    p="1"
+                  >
+                    Solution
+                  </Box>
+                  <Flex direction="column" fontSize="md" gap="2">
+                    <Box _hover={{ fontWeight: "bold" }}>Small</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>Large</Box>
+                    <Box _hover={{ fontWeight: "bold" }}>
+                      View all solutions
+                    </Box>
+                  </Flex>
+                </Flex>
+              </Grid>
+            </Box>
+          </Link>
+        </MenuList>
+      </Menu>
+
+
     </Flex>
   );
 }
