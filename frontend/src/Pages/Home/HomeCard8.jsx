5e8f745e4f54171e0ef05c9acc3d7fde5fb1d780
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-16 16:25:13 +0530

    File Added

--- frontend/src/Pages/Home/HomeCard8.jsx
+++ frontend/src/Pages/Home/HomeCard8.jsx
@@ -4,25 +4,49 @@ import { AddIcon, MinusIcon } from "@chakra-ui/icons"
 
 const HomeCard8 = ({type}) => {
   return (
-    <Box>
-        <Grid>
+    <Box w="100%" bg="#000042" >
+        <Grid 
+          templateColumns={{
+            base: "repeat(1,1fr",
+            sm: "repeat(1,1fr)",
+            md: "repeat(2,1fr)",
+            lg: "repeat(3,1fr)"
+          }}
+          w={{ lg: "92%", md:"90%", sm:"90%", base:"87%" }}
+          m="auto"
+          color="whiteAlpha.900"
+          >
             {type.map((i) => (
                 <Box>
-                    <Accordion>
+                    <Accordion allowMultiple borderColor="#000042" >
                         <AccordionItem>
                             {({isExpanded}) => (
                                 <>
                                 <h2>
-                                  <AccordionButton>
-                                    <Box>
+                                  <AccordionButton borderBottom="1px solid white">
+                                    <Box
+                                      as='span'
+                                      flex="1"
+                                      textAlign="left"
+                                      fontSize="20px"
+                                      ml={{ lg:"-4%", sm:"0%", base:"0%" }}
+                                    >
                                         {i.head}
                                     </Box>
                                     {isExpanded ? (
-                                        <Box>
+                                        <Box 
+                                         border="2px solid white"
+                                         borderRadius="100px"
+                                         p="2px 8px"
+                                         >
                                             <MinusIcon fontSize="12px" fontWeight="700" />
                                         </Box>
                                     ) : (
-                                        <Box>
+                                        <Box
+                                         border="2px solid white"
+                                         borderRadius="100px"
+                                         p="2px 8px"
+                                        >
                                             <AddIcon fontSize="12px" fontWeight="700" />
                                         </Box>
                                     )}
