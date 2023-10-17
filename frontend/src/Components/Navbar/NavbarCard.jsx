50da5ce9bfc90587175403f1410256e8ec55037f
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-17 16:30:18 +0530

    File Added

--- frontend/src/Components/Navbar/NavbarCard.jsx
+++ frontend/src/Components/Navbar/NavbarCard.jsx
@@ -1,10 +1,13 @@
-import { Box, Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Image, Input, Popover, PopoverBody, PopoverContent, PopoverTrigger, Text } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import { NavbarDetail1 } from "./NavbarDetail";
 import NavbarCard5 from "./NavbarCard5";
 import {AuthContext} from "../../ContextApi/AuthContext";
-import {Link, useNavigate} from "react-router-dom"
-import {} from "";
+import {Link, Navigate, useNavigate} from "react-router-dom"
+import {FiPhoneCall} from "react-icons/fi";
+import { TriangleDownIcon } from "@chakra-ui/icons";
+import { CiHeart } from "react-icons/ci";
+import { CgShoppingCart} from "react-icons/cg";
 
 
 export const NavbarCard1 = () =>{
@@ -35,17 +38,108 @@ export const NavbarCard2 = () => {
                     <Image src="https://i.imgur.com/OHxtfjd.png" alt="logo" w="75%" />
                     </Link>
                 </Box>
-                <HStack>
-                    <Box>
-                        <HStack>
-                            
+                <HStack w="85%" m="auto">
+                    <Box w="15%">
+                        <HStack fontSize="18px" fontWeight="bold">
+                            <FiPhoneCall />
+                            <Text>
+                                1800-111-111
+                            </Text>
                         </HStack>
                     </Box>
+                    <Box w="55%">
+                        <Input 
+                          placeholder="What are you looking for"
+                          border="1px solid black"
+                          w="95%"
+                          fontSize="17px"
+                          h="45px"
+                        />
+                    </Box>
+                    <HStack w="35%">
+                        <Button
+                         size="lg"
+                         bg="whiteAlpha.200"
+                         fontSize="14px"
+                         fontWeight="400"
+                         onClick={() => navigate("/")}
+                        >
+                            Track Order
+                        </Button>
+                        {isAuth === true ? (
+                            <Popover trigger="hover">
+                                <PopoverTrigger>
+                                    <Box
+                                     fontWeight={"600"}
+                                     fontSize="15px"
+                                     m="auto"
+                                     mt="-2px"
+                                     w="90px"
+                                     textAlign="center"
+                                     >
+                                        {AuthData[0].first_name}
+                                        <TriangleDownIcon
+                                          ml="2px"
+                                          fontSize="9px"
+                                          _hover={{transform: "rotate(180deg"}}
+                                        />
+                                    </Box>
+                                </PopoverTrigger>
+                                <PopoverContent
+                                 w="120px"
+                                 boxShadow={"rgba(0,0,0,0.24) 0px 3px 8px "}
+                                 >
+                                    <PopoverBody
+                                      h={"40px"}
+                                      pl="6"
+                                      fontSize="15px"
+                                      _hover={{fontWeight: "bold"}}
+                                    >
+                                        <Box
+                                          color="#333368"
+                                          onClick={() => {
+                                            setIsAuth(false);
+                                            return <Navigate to="/" />
+                                          }}
+                                        >
+                                            Sign Out
+                                        </Box>
+                                    </PopoverBody>
+                                </PopoverContent>
+                            </Popover>
+                        ) : (
+                            <Box display={"flex"}>
+                               
+                               
+                            </Box>
+                        )}
+                        <Button 
+                          leftIcon={<CiHeart />}
+                          size="lg"
+                          bg="whiteAlpha.900"
+                          fontSize="14px"
+                          fontWeight="400"
+                          onClick={() => navigate("/")}
+                        >
+                            WishList
+                        </Button>
+                        <Link to="/">
+                        <Button
+                          leftIcon={<CgShoppingCart />}
+                          size="lg"
+                          bg="whiteAlpha.900"
+                          fontSize="14px"
+                          fontWeight="400"
+                        >
+                            Cart
+                        </Button>
+                        </Link>
+                    </HStack>
                 </HStack>
             </HStack>
         </Box>
-    )
-}
+    );
+};
 
 
 
