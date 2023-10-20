0d485ec67173e824096a463f49eeb88603dc07b2
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-21 00:21:04 +0530

    File Added

--- frontend/src/Pages/Checkout/Checkout.jsx
+++ frontend/src/Pages/Checkout/Checkout.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {addToOrder} from "../../Redux/Order/order.action";
 import {cartReset} from "../../Redux/CartPage/action";
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, HStack, Image, Spacer, Switch } from "@chakra-ui/react";
 import Navbar, {} from "../../Components/Navbar/Navbar"
 
 
@@ -35,6 +35,30 @@ const Orders = () => {
   return (
     <Box m="auto">
         <Navbar />
+        <Box w="90%" m="auto">
+            <HStack spacing={"100px"} mt="15px" mb="20px" w="100%" gap="2">
+                <HStack>
+                    <Image  
+                      src="https://static.lenskart.com/media/desktop/img/25-July-19/whatsapp.png"
+                      boxSize="30px"
+                    />
+                    <Box fontSize={{lg:"16px", base: "sm"}} fontWeight="400">
+                        Get Orders Updates on Whatsapp
+                    </Box>
+                    <Switch size="md" />
+                    <Spacer />
+                </HStack>
+            </HStack>
+            <Box>
+                <Box>
+                    <Grid>
+                        <Grid>
+                            
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Box>
     </Box>
   );
 }
