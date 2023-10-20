f4505d03df48da87d2153f23bec7d18f7053def2
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-21 03:08:49 +0530

    File Added

--- frontend/src/Pages/Wishlist/Wishlist.jsx
+++ frontend/src/Pages/Wishlist/Wishlist.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { removeFromWishlist } from '../../Redux/Wishlist/wishlist.action';
 import { addToCart } from '../../Redux/CartPage/action';
-import { Box, Grid, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Grid, Heading, Img, Text } from '@chakra-ui/react';
 import Navbar from '../../Components/Navbar/Navbar';
 
 const Wishlist = () => {
@@ -63,14 +63,142 @@ const Wishlist = () => {
                 </Text>
             ) : (
                 <Box>
-                    <Grid>
-                        
+                    <Grid templateColumns="repeat(1,1fr)" gap={18} w={"100%"}>
+                        {wishlistItems &&
+                          wishlistItems &&
+                           wishlistItems.map((item) => (
+                            <Box
+                            key={item.id}
+                            borderWidth="1px"
+                            boxShadow="2xl"
+                            p="4"
+                            my="4"
+                            w={{ lg: "80%", md: "90%", sm: "90%", base: "95%" }}
+                            m="auto"
+                            >
+                                <Grid
+                                  m="auto"
+                                  templateColumns={{
+                                    base: "repeat(1,1fr)",
+                                    sm: "repeat(1,1fr)",
+                                    md: "repeat(1,1fr)",
+                                    lg: "60% 40%",
+                                    xl: "70% 30%"
+                                  }}
+                                  justify="space-between"
+                                  mb="2"
+                                >
+                                   <Text
+                                     fontSize="xl"
+                                     fontWeight="bold"
+                                     textTransform="capitalize"
+                                     mb={{ sm: "4", base: "4" }}
+                                   >
+                                    {item.productRefLink}
+                                   </Text>
+                                   <Grid
+                                      m={{ lg: "auto", sm: "left", base: "right" }}
+                                      templateColumns={{
+                                        base: "repeat(1,1fr)",
+                                        sm: "repeat(2,1fr)",
+                                        md: "repeat(2,1fr)",
+                                        lg: "repeat(2,1fr)",
+                                        xl: "repeat(2,1fr)"
+                                      }}
+                                      gap="4"
+                                      justify="space-between"
+                                      mb="2"
+                                   >
+                                    <Button
+                                      colorScheme="red"
+                                      onClick={() => handleAddToCart(item)}
+                                    >
+                                        Add to Cart
+                                    </Button>
+                                    <Button
+                                      colorScheme="red"
+                                      onClick={() => handleDelete(item)}
+                                    >
+                                        Remove
+                                    </Button>
+                                   </Grid>
+                                </Grid>
+
+                                <Grid
+                                   m="auto"
+                                   templateColumns={{
+                                     base: "repeat(1,1fr)",
+                                     sm: "40% 50%",
+                                     md: "30% 60%",
+                                     lg: "30% 60%",
+                                     xl: "20% 60%"
+                                   }}
+                                   align="center"
+                                   mb="1"
+                                >
+                                    <Img 
+                                      src={item.imageTsrc}
+                                      alt={item.name}
+                                      boxSize="180px"
+                                      m="auto"
+                                    />
+
+                                    <Box
+                                       ml="4"
+                                       textAlign={{
+                                         lg: "left",
+                                         md: "left",
+                                         sm: "left",
+                                         base: "center"
+                                       }}
+                                    >
+                                        <Text
+                                           fontSize="lg"
+                                           fontWeight="bold"
+                                           textTransform="capitalize"
+                                        >
+                                            {item.name}
+                                        </Text>
+
+                                        <Text fontSize="lg" fontWeight="bold">
+                                             Price : ₹ {item.price}.00 /-
+                                        </Text>
+                                        <Text
+                                           fontSize="lg"
+                                           fontWeight="bold"
+                                           color="gray.600"
+                                           textTransform="capitalize"
+                                        >
+                                             {item.productType}
+                                        </Text>
+
+                                        <Text
+                                           fontSize="lg"
+                                           fontWeight="bold"
+                                           color="gray.600"
+                                           textTransform="capitalize"
+                                        >
+                                            Colour : {item.colors}
+                                        </Text>{" "}
+                                        <Text
+                                            fontSize="md"
+                                            fontWeight="600"
+                                            color="gray.600"
+                                            textTransform="capitalize"
+                                         >
+                                              Shape : {item.shape}
+                                        </Text>
+                                    </Box>
+                                </Grid>
+                            </Box>
+                           ))
+                        }
                     </Grid>
                 </Box>
             )}
         </Box>
     </Box>
   );
-}
+};
 
 export default Wishlist;
