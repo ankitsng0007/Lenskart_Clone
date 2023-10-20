6872d2b90becf533e92f408e28d1c9ff09f58137
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-20 23:59:30 +0530

    File Added

--- frontend/src/Pages/Checkout/Checkout.jsx
+++ frontend/src/Pages/Checkout/Checkout.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const Checkout = () => {
+
+const Orders = () => {
+    const navigate = useNavigate();
+    const { cart, coupon } = useSelector((state) => state.cartReducer );
+    const dispatch = useDispatch();
+
+    const getTotalPrice = () => {
+        const totalPrice = cart.reduce(
+            (acc, item) => acc + item.price * item.quantity,
+            0
+        );
+        return totalPrice;
+    };
+
+    const 
+
   return (
     <div>
       
@@ -8,4 +25,4 @@ const Checkout = () => {
   );
 }
 
-export default Checkout;
+export default Orders;
