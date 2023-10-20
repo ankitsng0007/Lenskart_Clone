620a1922dcd7061ca0261aa5e62e19ca4b1e577b
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-21 00:12:16 +0530

    File Added

--- frontend/src/Pages/Checkout/Checkout.jsx
+++ frontend/src/Pages/Checkout/Checkout.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import {addToOrder} from "../../Redux/Order/order.action";
+import {cartReset} from "../../Redux/CartPage/action";
+import { Box } from "@chakra-ui/react";
+import Navbar, {} from "../../Components/Navbar/Navbar"
 
 
 const Orders = () => {
@@ -16,12 +20,22 @@ const Orders = () => {
         return totalPrice;
     };
 
-    
+    const handleClick = () =>{
+        dispatch(addToOrder(cart));
+        navigate("/confirm");
+        dispatch(cartReset());
+    };
+
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, "0");
+    const day = today.getDate().toString().padStart(2, "0");
+    const currentDate = `${day}-${month}-${year}`;
 
   return (
-    <div>
-      
-    </div>
+    <Box m="auto">
+        <Navbar />
+    </Box>
   );
 }
 
