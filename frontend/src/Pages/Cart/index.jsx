731baba458222f98272176f45519b5768ec1f3ee
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-20 13:37:43 +0530

    File Added

--- frontend/src/Pages/Cart/index.jsx
+++ frontend/src/Pages/Cart/index.jsx
@@ -1,10 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import CartLength from './CartLength';
 import Navbar from '../../Components/Navbar/Navbar';
 import  CartItem from "./CartItem";
+import PriceDetail from './PriceDetail';
+import SaleBox from './SaleBox';
+import CouponBox from './CouponBox';
+import CartEmpty from "./CartEmpty";
+import Footer from '../../Components/Footer/Footer';
 
 
 const CartPage = () => {
@@ -63,8 +68,54 @@ const CartPage = () => {
                 <CartLength cartLength={cart.length} />
                 <CartItem />
             </Flex>
+            <Flex
+              flexDirection={"column"}
+              border={"0px solid blue"}
+              width={{
+                base: "95%",
+                sm: "95%",
+                md: "95%",
+                lg: "35%",
+                xl: "27%",
+                "2xl": "27%"
+              }}
+              gap={"5"}
+            >
+                <Text
+                  fontSize="20px"
+                  fontFamily="sans-serif"
+                  border={"0px solid red"}
+                  fontWeight={500}
+                >
+                    Bill Details
+                </Text>
+                <PriceDetail 
+                  totalPrice={getTotalPrice()}
+                  discountPrice={getdiscountPrice()}
+                />
+                <SaleBox />
+
+                <CouponBox />
+
+                <Button
+                   backgroundColor={"#12daac"}
+                   color="#091e52"
+                   borderRadius={"20px"}
+                   padding="16px 24px 16px 24px"
+                   fontSize={"16px"}
+                   height="56px"
+                   fontWeight={"700"}
+                   onClick={() => navigate("/shipping")}
+                >
+                    Proceed To Checkout
+                </Button>
+            </Flex>
         </Flex>
+    ) : (
+        <CartEmpty />
     )}
+    
+    <Footer />
     </>
   );
 }
