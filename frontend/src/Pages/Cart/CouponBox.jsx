28e8dd89cf6cb012a8c25c887488b344ab65879f
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-20 12:17:54 +0530

    File Added

--- frontend/src/Pages/Cart/CouponBox.jsx
+++ frontend/src/Pages/Cart/CouponBox.jsx
@@ -1,4 +1,4 @@
-import { useToast } from '@chakra-ui/react';
+import { Box, Button, Flex, Input, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useDispatch } from "react-redux";
 
@@ -31,9 +31,55 @@ const CouponBox = () => {
     };
 
   return (
-    <div>
-      
-    </div>
+    <Flex
+    padding="10px"
+    border={"1px solid grey"}
+    borderRadius="10px"
+    boxShadow={"lg"}
+    cursor={"pointer"}
+    flexDirection="column"
+    >
+        <Flex justifyContent={"space-between"} gap="5" >
+            <Box
+              w={{
+                xl: "70%",
+                lg: "60%",
+                md: "80%",
+                sm: "80%",
+                base: "70%"
+              }}
+            >
+                <Input 
+                  type='text'
+                  placeholder='Enter coupon code'
+                  value={couponCode}
+                  onChange={(e) => setCouponCode(e.target.value)}
+                  h={"12"}
+                  fontSize="16px"
+                />
+            </Box>
+            <Box
+               w={{
+                xl: "30%",
+                lg: "40%",
+                md: "20%",
+                sm: "20%",
+                base: "30%"
+               }}
+            >
+                <Button
+                  color="white"
+                  backgroundColor={"#12daac"}
+                  fontSize={"16px"}
+                  padding="10px"
+                  mt={"5px"}
+                  onClick={handleSubmit}
+                >
+                    Apply
+                </Button>
+            </Box>
+        </Flex>
+    </Flex>
   );
 }
 
