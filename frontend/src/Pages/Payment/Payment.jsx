1bd785ca48e87cf7fddb015ef21c728fc300fc37
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-21 11:57:41 +0530

    File added

--- frontend/src/Pages/Payment/Payment.jsx
+++ frontend/src/Pages/Payment/Payment.jsx
@@ -1,7 +1,10 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Grid, Image } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { addToOrder } from '../../Redux/Order/order.action';
+import { cartReset } from '../../Redux/CartPage/action';
+import Navbar from '../../Components/Navbar/Navbar';
 
 const Payment = () => {
     const init = {
@@ -80,12 +83,128 @@ const Payment = () => {
     }
   };
 
-        }
-    }
+        const handleClick = () => {
+            dispatch(addToOrder(cart));
+            navigate("/confirm");
+            dispatch(cartReset());
+        };
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <Navbar />
+    <Box>
+        <br />
+        <br/>
+        <Box>
+            <Box
+              w={{ xl: "75%", lg: "80%", md: "90%", sm: "90%", base: "95%" }}
+              m="auto"
+            >
+                <Box
+                  m="auto"
+                  boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}
+                  borderRadius="lg"
+                >
+                    <Box
+                      bg="#00bac6"
+                      color={"white"}
+                      fontWeight="700"
+                      p="4px 0px 6px 6px "
+                      fontSize="xl"
+                      textAlign="left"
+                    >
+                        PAYMENT OPTION
+                    </Box>
+                    <br/>
+                    <Box>
+                        <Flex
+                          w="200px"
+                          flexDirection="column"
+                          borderRight="2px solid gray"
+                          borderBottom="2px solid gray"
+                          borderRadius="2xl"
+                          display={{ md: "inherit", base: "none" }}
+                        >
+                            <Box
+                              p="16px 0px 16px 16px"
+                              fontWeight={"500"}
+                              _hover={{ bg: "blackAlpha.200" }}
+                              bg="blackAlpha.200"
+                            >
+                                Credit/Debit Card
+                            </Box>
+                            <Box
+                              p="16px 0px 16px 16px"
+                              fontWeight={"500"}
+                              _hover={{ bg: "blackAlpha.200" }}
+                            >
+                                BHIM/UPI PhonePe
+                            </Box>
+                            <Box
+                              p="16px 0px 16px 16px"
+                              fontWeight={"500"}
+                              _hover={{ bg: "blackAlpha.200" }}
+                            >
+                                Net Banking
+                            </Box>
+                            <Box
+                              p="16px 0px 16px 16px"
+                              fontWeight={"500"}
+                              _hover={{ bg: "blackAlpha.200" }}
+                            >
+                                UPI QR Code
+                            </Box>
+                            <Box
+                              p="16px 0px 16px 16px"
+                              fontWeight={"500"}
+                              _hover={{ bg: "blackAlpha.200" }}
+                            >
+                                Paytm
+                            </Box>
+                        </Flex>
+                        <Box m="10px 10px 10px 10px">
+                            <Grid
+                              templateColumns={{
+                                base: "repeat(1,1fr)",
+                                sm: "repeat(1,1fr)",
+                                md: "20% 75%",
+                                lg: "20% 75%",
+                                xl: "20% 80%"
+                              }}
+                              fontSize="lg"
+                              justifyContent={{
+                                md: "left",
+                                sm: "center",
+                                base: "center"
+                              }}
+                            >
+                                <Box
+                                  fontWeight="bold"
+                                  color="gray.600"
+                                  display={{ md: "inherit", base: "none" }}
+                                >
+                                    100% Secure
+                                </Box>
+                                <Image 
+                                    ml={{ md: "80px", sm: "0px", base: "0px" }}
+                                    h={{ xl: "40px", lg: "40px", base: "40px" }}
+                                    src="https://static5.lenskart.com/images/cust_mailer/Mar-03/CheckoutStrip.png"
+                                    w={{
+                                      xl: "100%",
+                                      lg: "80%",
+                                      md: "80%",
+                                      sm: "100%",
+                                      base: "100%"
+                                    }}
+                                />
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    </Box>
+    </>
   );
 }
 
