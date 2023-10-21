a03c5938663a36519fd60d4844810cd2477533ea
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-22 02:56:59 +0530

    File added

--- frontend/src/Pages/Admin/Productlist.jsx
+++ frontend/src/Pages/Admin/Productlist.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { useToast } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Input, Select, useToast } from "@chakra-ui/react";
 
 
 const Productlist = () => {
@@ -34,14 +34,107 @@ const Productlist = () => {
   };
 
   useEffect(() => {
-    
-  })
+    fetchData();
+  }, [sort, filter, page, gender, shape, style, productref]);
+   
+  const handleDelete = async (id) => {
+    try{
+        const response = await fetch(
+            `https://harlequin-fawn-tutu.cyclic.app/product/${id}`,
+            {
+                method: "DELETE"
+            }
+        );
+        if(response.status === 200){
+            fetchData();
 
+            toast({
+                title: "Product Deleted",
+                description: "Product Deleted Successfully.",
+                status: "success",
+                duration: 2000,
+                isClosable: true
+            });
+        }
+    }catch(error){
+        console.log(error);
+    }
+  };
 
   return (
-    <div>
-      
-    </div>
+    <Box bg="gray.200" minH="710px">
+        
+        <br/>
+        <Grid
+          templateColumns={{
+            lg: "30% 10% 10% 10% 15% 15%",
+            md: "30% 10% 10% 10% 15% 15%",
+            sm: "repeat(1, 1fr)",
+            base: "repeat(1, 1fr)"
+          }}
+          justifyContent="space-between"
+          w="95%"
+          m="auto"
+          gap="2"
+        >
+            <GridItem>
+                <Input 
+                   size="lg"
+                   type="search"
+                   fontSize="16px"
+                   h="40px"
+                   bg="whiteAlpha.900"
+                   borderRadius="3xl"
+                   placeholder="Search by name"
+                   value={productref}
+                   onChange={(e) => setProductref(e.target.value)}
+                 />
+            </GridItem>
+            <GridItem>
+                <Select
+                  value={filter}
+                  onChange={(e) => setFilter(e.target.value)}
+                  size="lg"
+                  fontSize="16px"
+                  h="40px"
+                  bg="whiteAlpha.900"
+                >
+                <option value="">All Glasses</option>
+            <option value="sunglasses">Sun Glasses</option>
+            <option value="eyeglasses">Eye Glasses</option>
+                </Select>
+            </GridItem>
+            <GridItem>
+                <Select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  size="lg"
+                  fontSize="16px"
+                  h="40px"
+                  bg="whiteAlpha.900"
+                >
+                <option value="">For all Gender</option>
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kid">Kid</option>
+                </Select>
+            </GridItem>
+            <GridItem>
+          <Select
+            value={sort}
+            onChange={(e) => setSort(e.target.value)}
+            size="lg"
+            fontSize="16px"
+            h="40px"
+            bg="whiteAlpha.900"
+          >
+            <option value="">Sort By Price</option>
+            <option value="lowtohigh">Low to High</option>
+            <option value="hightolow">High to Low</option>
+          </Select>
+        </GridItem>
+        </Grid>
+    </Box>
   );
 }
 
