92f95b0353365fcd4acf8cc681aee94d94212120
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-24 00:56:18 +0530

    File Added

--- frontend/src/Components/Navbar/NavbarMob.jsx
+++ frontend/src/Components/Navbar/NavbarMob.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
-import {Avatar, Box, Button, Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, Image, Input, useDisclosure} from "@chakra-ui/react";
+import {Accordion, AccordionButton, AccordionIcon, AccordionItem, Avatar, Box, Button, Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, Heading, Image, Input, Text, useDisclosure} from "@chakra-ui/react";
 import {AuthContext} from "../../ContextApi/AuthContext";
 import {Link, useNavigate} from "react-router-dom";
 import {HamburgerIcon} from "@chakra-ui/icons";
+import Login from "../../Pages/Login/Login";
+import Signup from '../../Pages/SignUp/Signup';
 
 
 
@@ -69,10 +71,152 @@ const NavbarMob = () => {
                                    size="lg"
                                    mr="2"
                                 />
+                                <Flex
+                                  direction="column"
+                                  justifyContent="center"
+                                  alignItems="flex-start"
+                                >
+                        <Text mt="10px" fontSize="20px" color="blackAlpha.900">
+                          {Authdata[0].first_name}
+                        </Text>
+                        <Text color="gray.500" mt="5%" fontSize="sm">
+                          Enjoy Buy 1 Get 1 offer for 365 days
+                        </Text>
+                      </Flex>
                             </Flex>
+                    <Button
+                      w="100%"
+                      h="35px"
+                      mt="5%"
+                      colorScheme="blue"
+                      fontSize="15px"
+                      _hover={{ bg: "blue.400" }}
+                    >
+                      GET GOLD MEMBERSHIP
+                    </Button>
                         </Flex>
+                    ) : (
+                        <Box
+                          style={{
+                      padding: "5%",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "100%"
+                    }}
+                        >
+                            <Box
+                              style={{
+                              width: "100%",
+                              display: "flex",
+                              justifyContent: "space-evenly",
+                              marginBottom: "-6%"
+                            }}
+                            >
+                            <Box
+                              bg="blue.500"
+                              p="10px 15px"
+                              rounded="lg"
+                              _hover={{ bg: "blue.200" }}
+                            >
+                                <Login />
+                            </Box>
+                            <Box
+                        bg="blue.500"
+                        p="10px 15px"
+                        rounded="lg"
+                        _hover={{ bg: "blue.200" }}
+                      >
+                        <Signup />
+                      </Box>
+                            </Box>
+                        </Box>
                     )}
                 </DrawerHeader>
+                <DrawerBody borderBottomWidth="1px" bg="whiteAlpha.900">
+                    <Box display="flex" flexDirection="column" fontSize="16px" >
+                        <Link to="/orderhistory">
+                        <Box
+                      borderBottom="0.1px solid gray"
+                      fontSize="15px"
+                      p="4% 0%"
+                      color="black"
+                      _hover={{ fontWeight: "bold" }}
+                      onClick={() => navigate("/orderhistory")}
+                    >
+                      My Orders
+                    </Box>
+                        </Link>
+                        <Link to="/cart">
+                    <Box
+                      borderBottom="0.1px solid gray"
+                      fontSize="15px"
+                      p="4% 0%"
+                      color="black"
+                      _hover={{ fontWeight: "bold" }}
+                    >
+                      Cart
+                    </Box>
+                  </Link>
+                  <Link to="/wishlist">
+                    <Box
+                      borderBottom="0.1px solid gray"
+                      fontSize="15px"
+                      p="4% 0%"
+                      color="black"
+                      _hover={{ fontWeight: "bold" }}
+                    >
+                      Wishlist
+                    </Box>
+                  </Link>
+                  <Link>
+                    <Box
+                      borderBottom="0.1px solid gray"
+                      fontSize="15px"
+                      p="4% 0%"
+                      color="black"
+                      _hover={{ fontWeight: "bold" }}
+                    >
+                      Manage Notification
+                    </Box>
+                  </Link>
+                  <Link>
+                    <Box
+                      borderBottom="1px solid white"
+                      fontSize="15px"
+                      p="4% 0%"
+                      color="black"
+                      _hover={{ fontWeight: "bold" }}
+                    >
+                      Contact Us
+                    </Box>
+                  </Link>                     
+                </Box>
+
+                <Heading mt="15%" color="black" fontSize="15px" mb="5%">
+                  SHOP NOW
+                </Heading>
+                <Box display="flex" flexDirection="column" fontSize="16px">
+                    <Accordion defaultIndex={[0]} allowMultiple w="100%" m="auto">
+                    <AccordionItem>
+                        <h2>
+                            <AccordionButton>
+                            <Box
+                            as="span"
+                            flex="1"
+                            textAlign="left"
+                            fontWeight="500"
+                          >
+                            Men
+                          </Box>
+                          <AccordionIcon />
+                            </AccordionButton>
+                        </h2>
+                    </AccordionItem>
+                    </Accordion>
+                </Box>
+                </DrawerBody>
             </DrawerContent>
           </Drawer>
         </Box>
