8ca87dffdf8fe80ea0e880118b3bc8764eda235e
ankitsng0007 <ankitsng0007@gmail.com>
2023-10-24 00:35:02 +0530

    File Added

--- frontend/src/Components/Navbar/NavbarMob.jsx
+++ frontend/src/Components/Navbar/NavbarMob.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
-import {Box, HStack, Image, useDisclosure} from "@chakra-ui/react";
+import {Avatar, Box, Button, Drawer, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, Image, Input, useDisclosure} from "@chakra-ui/react";
 import {AuthContext} from "../../ContextApi/AuthContext";
 import {Link, useNavigate} from "react-router-dom";
+import {HamburgerIcon} from "@chakra-ui/icons";
+
 
 
 const NavbarMob = () => {
@@ -27,6 +29,53 @@ const NavbarMob = () => {
           </Link>
 
             </Box>
+             <Box w="70%" display={{ sm: "inherit", base: "none" }}>
+          <Input
+            placeholder="What are you looking for"
+            border="1px solid black"
+            w="90%"
+            fontSize="16px"
+            h="35px"
+          />
+        </Box>
+
+        <Box>
+            <Button colorScheme="blue" p="0" onClick={onOpen}>
+            <HamburgerIcon fontSize="20px" />
+          </Button>
+          <Drawer
+            size="xs"
+            isOpen={isOpen}
+            placement="right"
+            initialFocusRef={firstField}
+            onClose={onClose}
+          >
+            <DrawerOverlay />
+            <DrawerContent color="whiteAlpha.900">
+                <DrawerCloseButton />
+                <DrawerHeader bg="whiteAlpha.900">
+                    {isAuth ? (
+                        <Flex
+                          borderBottom="2px solid #18CFA8"
+                          p="5%"
+                          direction="column"
+                          justifyContent="center"
+                          alignItems="center"
+                          w="100%"
+                        >
+                            <Flex w="100%">
+                                <Avatar 
+                                   src="https://bit.ly/broken-link"
+                                   size="lg"
+                                   mr="2"
+                                />
+                            </Flex>
+                        </Flex>
+                    )}
+                </DrawerHeader>
+            </DrawerContent>
+          </Drawer>
+        </Box>
         </HStack>
     </Box>
   );
